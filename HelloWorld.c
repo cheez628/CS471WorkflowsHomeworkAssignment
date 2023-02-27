d236dc89919301ba7f3a43901ecfa87f2d871c89
Juan Gurrola <jahl628@gmail.com>
2023-02-26 17:43:34 -0700

    Added small prgram to convert from degrees C to F

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,4 +2,15 @@
 
 int main(){
 
+	float tempC = 0.0;
+	float tempF = 0.0;
+
+	printf("Input a temperature (in C): ");
+	scanf("%f", &tempC);
+
+	tempF = (tempC * 9 / 5) + 32;
+
+	printf("%f degrees Fahrenheit. \n", tempF);
+
+	return 0;
 }
